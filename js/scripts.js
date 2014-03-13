0b3ac485511a2c24c0a8e34946aa94f89457c0b4
Trent Petersen <Foxtrot@Foxtrot.local>
2014-03-13 10:01:52 -0700

    set up zip code

--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,41 @@ $(document).foundation();
 
 // Your Awesome Scripts!
 // Docs at http://simpleweatherjs.com
-$(document).ready(function() {
-  $.simpleWeather({
-    location: 'Spokane, WA',
-    woeid: '',
-    unit: 'f',
-    success: function(weather) {
-      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-      html += '<li class="currently">'+weather.currently+'</li>';
-      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
-  
-      $("#weather").html(html);
-    },
-    error: function(error) {
-      $("#weather").html('<p>'+error+'</p>');
-    }
-  });
-});
+$(document).ready(function(){
 
-	console.log('Page Loaded. Lets Do this!');
+$('.button').click(function(){
 
+      var getZip = $('input').val();
+      console.log(getZip);
+    
+  //Simple Weather
+    $.simpleWeather({  
+      zipcode: getZip,
+      location: '',
+      unit: 'f',
+      success: function(weather) {
+        
+      //Get weather data
+        html = '<h3>'+weather.city+', '+weather.region+'</h3>';
+        html += '<h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+        html += '<ul><li><img src="img/'+weather.code+'.png"></li>';
+        html += '<li><h5>'+weather.currently+'</h5></li></ul>';
+        
+        
+        $('body').addClass('bg' + weather.code);
+      
+      //Put weather data, calling simple weather
+        $("#weather").html(html);
+      },
+      error: function(error) {
+        $("#weather").html('<p>'+error+'</p>');
+      }
+      });
+
+  }); //Button click function *end*
+
+
+
+  console.log('Page Loaded. Lets Do this!');
+
+}); 
