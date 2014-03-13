a095f6515c35bf3bc398e440e404e156812e22f1
Trent Petersen <Foxtrot@Foxtrot.local>
2014-03-13 09:04:48 -0700

    Revert 26efcb0..b68f120
    This rolls back to commit 26efcb03f85a371809494caa23a1f601e1fc32b2.

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,26 @@
 // Make Foundation Go!
-//$(document).foundation();
+$(document).foundation();
 
 // Your Awesome Scripts!
-$(document).ready(function(){
-
-	$.simpleWeather({
-    
-    location: '99203',
-    
+// Docs at http://simpleweatherjs.com
+$(document).ready(function() {
+  $.simpleWeather({
+    location: 'Spokane, WA',
+    woeid: '',
+    unit: 'f',
     success: function(weather) {
-      
-      // Get & Store Weather Data
-      // html = '<h2><i class="icon-' + weather.code+'"></i> ' + weather.temp +'&deg;' + weather.units.temp+'</h2>';
-      var temp, tomHi, tomLo, cityAndState; 
-
-      temp = weather.temp + '<span> f</span>';
-      tomHi = weather.tomorrow.high;
-      tomLo = weather.tomorrow.low;
-      cityAndState = weather.city + ' , ' + weather.region;
-
-      console.log(cityAndState);
-
-      // Display Weather
-      $('.temp').html(temp);
-      $('.tomHi').html(tomHi);
-      $('.tomLo').html(tomLo);
-      $('.cityAndState').html(cityAndState);
-
+      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+  
+      $("#weather").html(html);
     },
-    
     error: function(error) {
       $("#weather").html('<p>'+error+'</p>');
     }
-  
   });
-
-
+});
 
 	console.log('Page Loaded. Lets Do this!');
 
-}); 
